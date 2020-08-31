a3ba55d863c212d5a416fcc530814bbfdff9fbd4
awebmaster <vloac2013@yandex.ru>
2020-08-31 16:27:00 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace DW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Используйте параметры: dw.exe [путь к папке]");
+                return;
+            }
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = args[0];
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Бортжурнал веб-разработчика awebmaster.ru");
+            Console.WriteLine("Мониторинг работает...");
+            Console.WriteLine("Для остановки нажмите Ctrl+Break");
+            while (true) Thread.Sleep(1000);
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            string fileOrDirectory;
+            string fileID = FileID.getFileUniqueSystemID(e.FullPath);
+
+            if (e.ChangeType == WatcherChangeTypes.Deleted) fileOrDirectory = "unknown";
+            else if (Directory.Exists(e.FullPath)) fileOrDirectory = "directory";
+            else fileOrDirectory = "file";
+
+            Console.WriteLine("{0}|{1}|{2}|{3}", e.ChangeType, fileOrDirectory, fileID, e.FullPath);
+            return;
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            string fileOrDirectory;
+            string fileID = FileID.getFileUniqueSystemID(e.FullPath);
+
+            if (Directory.Exists(e.FullPath)) fileOrDirectory = "directory";
+            else fileOrDirectory = "file";
+
+            Console.WriteLine("{0}|{1}|{2}|{3}|{4}", e.ChangeType, fileOrDirectory, fileID, e.OldFullPath, e.FullPath);
+
+            return;
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# dw
+Утилита для мониторинга за изменениями директории
+
+Пример запуска
+dw.exe [c:\Temp]
+
+Forked from @jpmoura/DirectoryWatcher
